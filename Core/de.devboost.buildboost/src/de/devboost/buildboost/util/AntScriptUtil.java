abcb47ad1af2c66c9f08f1fa6b13eaa66a63f5ae
Thomas Schubert <funthomas424242@googlemail.com>
2013-02-27 21:58:26 +0100

    merged with upstream

--- Core/de.devboost.buildboost/src/de/devboost/buildboost/util/AntScriptUtil.java
+++ Core/de.devboost.buildboost/src/de/devboost/buildboost/util/AntScriptUtil.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2012
+ * Software Technology Group, Dresden University of Technology
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany;
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
 package de.devboost.buildboost.util;
 
 import java.io.File;
@@ -12,8 +27,21 @@ public class AntScriptUtil {
 			content.append("<unzip src=\"" + file.getAbsolutePath() + "\" dest=\"" + targetDir.getAbsolutePath() + "\" />");			
 		} else {
 			content.append("<exec executable=\"tar\" dir=\"" + targetDir.getAbsolutePath() +  "\" failonerror=\"true\">");
-			content.append("<arg value=\"-zxf\"/>");
+			content.append("<arg value=\"zxf\"/>");
 			content.append("<arg value=\"" + file.getAbsolutePath() + "\"/>");
+			content.append("</exec>");
+		}
+	}
+
+	public static void addZipFileCompressionScript(XMLContent content,
+			String zipFile, String folderToZip) {
+		if (zipFile.endsWith(".zip")) {
+			content.append("<zip destfile=\"" + zipFile  + "\" basedir=\""+ folderToZip + "\" />");
+		} else {
+			content.append("<exec executable=\"tar\" dir=\""+ folderToZip + "\" failonerror=\"true\">");
+			content.append("<arg value=\"cvzf\"/>");
+			content.append("<arg value=\"" + zipFile + "\"/>");
+			content.append("<arg value=\".\"/>");
 			content.append("</exec>");
 		}
 	}
